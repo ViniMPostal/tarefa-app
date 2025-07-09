fc41ece9a963337e9bab1b0177c367986e0f09e2
ViniMPostal <viniciusmppostal@gmail.com>
2025-07-09 11:11:08 -0300

    FEAT: ajuste no banco

--- db.py
+++ db.py
@@ -1,8 +1,18 @@
 import psycopg2
+import os
 
 def get_connection():
+    ambiente = os.getenv("AMBIENTE", "desenvolvimento")
+
+    if ambiente == "homologacao":
+        dbname = "tarefa_homologacao"
+    elif ambiente == "producao":
+        dbname = "tarefa_producao"
+    else:
+        dbname = "tarefa_db"  # local (dev)
+
     return psycopg2.connect(
-        dbname='tarefa_db',
+        dbname=dbname,
         user='vinicius',
         password='senha',
         host='localhost'
